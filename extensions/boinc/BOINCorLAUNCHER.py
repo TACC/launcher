9a9daf5323d0bb0170091796c8cb8f5b3b18cdc5
Marshall Lerner <marshall_lerner@brown.edu>
2017-08-03 10:36:29 -0500

    changing the leading whitespaces from 4 spaces to 2

--- extensions/boinc/BOINCorLAUNCHER.py
+++ extensions/boinc/BOINCorLAUNCHER.py
@@ -4,21 +4,21 @@ import sys
 from can_offload import *
 
 if len(sys.argv) < 2 or len(sys.argv) > 2:
-    raise EnvironmentError("Too many or not enough variables. Exiting")
-    exit(1)
+  raise EnvironmentError("Too many or not enough variables. Exiting")
+  exit(1)
 else:
-    file_object = open(sys.argv[1])
-    bout = open("BOINCcommands.txt", "w")
-    lout = open("launchercommands.txt", "w")
-    for line in file_object.readlines():
-        try:
-            if can_offload(line.split()[0]):
-                bout.write(line)
-            else:
-                lout.write(line)
-        except IndexError:
-            print("There was a line with no command. Skipping this line.")
-        except CommandNotFoundException:
-            print("The line `%s` contained an error with the command. Skipping this line." %line[:-1])
+  file_object = open(sys.argv[1])
+  bout = open("BOINCcommands.txt", "w")
+  lout = open("launchercommands.txt", "w")
+  for line in file_object.readlines():
+    try:
+      if can_offload(line.split()[0]):
+        bout.write(line)
+      else:
+        lout.write(line)
+    except IndexError:
+      print("There was a line with no command. Skipping this line.")
+    except CommandNotFoundException:
+      print("The line `%s` contained an error with the command. Skipping this line." %line[:-1])
 bout.close()
 lout.close()
--- extensions/boinc/can_offload.py
+++ extensions/boinc/can_offload.py
@@ -5,29 +5,29 @@ import subprocess
 import commands
 
 class DynamicNonStandardException(Exception): #change this exception depending on what the actual error is for
-    pass
+  pass
 
 class CommandNotFoundException(Exception): #if the command is not found
-    pass
+  pass
 
 def can_offload(fileName):
-    try:
-        good_path = subprocess.check_output(["which", fileName])[:-1]
-        if "ELF" in subprocess.check_output(["file", good_path]):
-            #os.access(fileName, os.X_OK)
-            try:
-                libs = subprocess.check_output(["ldd", good_path])
-            except subprocess.CalledProcessError:
-                return True
-            libArr = libs.split("\n")
-            for lineNum in range(1, len(libArr) - 1):
-                if any(possibilities in libArr[lineNum] for possibilities in ("/lib/", "/lib64/", "/usr/")):
-                        continue
-            #    raise DynamicNonStandardException("%s contains more than just standard libraries %s" %(fileName, libArr[lineNum]))
-            #   I wasn't sure if there should be an error thrown or it should just return false.
-                return False
-            return True
-        else:
-            return False
-    except subprocess.CalledProcessError:
-        raise CommandNotFoundException("Command was not found")
+  try:
+    good_path = subprocess.check_output(["which", fileName])[:-1]
+    if "ELF" in subprocess.check_output(["file", good_path]):
+      #os.access(fileName, os.X_OK)
+      try:
+        libs = subprocess.check_output(["ldd", good_path])
+      except subprocess.CalledProcessError:
+        return True
+      libArr = libs.split("\n")
+      for lineNum in range(1, len(libArr) - 1):
+        if any(possibilities in libArr[lineNum] for possibilities in ("/lib/", "/lib64/", "/usr/")):
+          continue
+      #    raise DynamicNonStandardException("%s contains more than just standard libraries %s" %(fileName, libArr[lineNum]))
+      #   I wasn't sure if there should be an error thrown or it should just return false.
+        return False
+      return True
+    else:
+      return False
+  except subprocess.CalledProcessError:
+    raise CommandNotFoundException("Command was not found")
